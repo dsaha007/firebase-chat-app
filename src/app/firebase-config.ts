fc1897041222ad131979e7382a8ac617e0c291e6
Rizwan Haque <84337224+bashaquerizwan@users.noreply.github.com>
2025-04-12 20:12:32 +0530

    Update firebase-config.ts

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAGIKJax6L6ItVgNrVFBSrO2AlAB4NnBA4',
-  authDomain: 'chat-app-bassetti-event.firebaseapp.com',
-  projectId: 'chat-app-bassetti-event',
-  storageBucket: 'chat-app-bassetti-event.firebasestorage.app',
-  messagingSenderId: '443539694562',
-  appId: '1:443539694562:web:cd5245d9acc39c7c770fbb',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
